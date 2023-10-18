edcb1e19d35df50e99e046dba5062bee0075b21b
Marcin Rządkowski <marcin.rzadkowski@microsoft.wsei.edu.pl>
2023-10-18 21:04:43 +0200

    lab2

---
+++ Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Laboratorium_2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calculator([FromQuery(Name = "operator")] Operators op, double? a, double? b) 
+        {
+            if (a == null || b == null || op == null)
+            {
+                return View("Error");
+            }
+            switch (op)
+            {
+                case Operators.ADD:
+                    ViewBag.op = a + b;
+                    break;
+                case Operators.SUB:
+                    if (a > b)
+                        ViewBag.op = a - b;
+                    else
+                        ViewBag.op = b - a;
+                    break;
+                case Operators.MUL:
+                    ViewBag.op = a * b;
+                    break;
+                case Operators.DIV:
+                    if (a > b)
+                        ViewBag.op = a / b;
+                    else
+                        ViewBag.op = b / a;
+                    break;
+            }
+            ViewBag.op = a + b;
+            return View();
+        }
+    }
+}
---
+++ Calculator/Controllers/Operators.cs
@@ -0,0 +1,4 @@
+﻿public enum Operators
+{
+    UNKNOWN, ADD, SUB, MUL, DIV
+}
