0ea8a171055326cacd8465a4580a7a26670b5778
Marcin Rządkowski <marcin.rzadkowski@microsoft.wsei.edu.pl>
2023-10-24 21:53:56 +0200

    zadanie birth

---
+++ Laboratorium 2/Controllers/BirthController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Laboratorium_2.Models;
+
+namespace Laboratorium_2.Controllers
+{
+    public class BirthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Result(Birth model)
+        {
+            if (!model.IsValid())
+            {
+                return BadRequest();
+            }
+            return View(model);
+        }
+    }
+}   
---
+++ Laboratorium 2/Models/Birth.cs
@@ -0,0 +1,18 @@
+﻿namespace Laboratorium_2.Models
+{
+    public class Birth
+    {
+        public DateTime Date { get; set; }
+        public string? Name { get; set; }
+
+        public bool IsValid()
+        {
+            return Date != null && Name != null;
+        }
+
+        public int Calculate()
+        {
+            return DateTime.Now.Year - Date.Year;
+        }
+    }
+}
